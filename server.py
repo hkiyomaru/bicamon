2aa3de9bb69715a6ff3767af73a516089ec5c05d
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-03-11 13:18:19 +0900

    Add script to handle database

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 
 app = Flask(__name__)
 DATABASE = 'db/cells.db'
@@ -7,7 +7,8 @@ DATABASE = 'db/cells.db'
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = connect_to_database()
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = make_dicts
     return db
 
 def close_connection(exception):
@@ -15,8 +16,20 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query ,args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 @app.route('/')
 def index():
+    # for cell in query_db('select * from cells'):
+    #     print cell['name']
     return render_template('index.html')
 
 if __name__ == '__main__':

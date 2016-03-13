6fc1dd5939def02f58a41ac45f2bba864791f57c
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-03-13 23:00:01 +0900

    Add script to build database

--- db/scripts/createTable.py
+++ db/scripts/createTable.py
@@ -3,18 +3,10 @@ import sqlite3
 
 conn = sqlite3.connect('cells.db')
 
-sql = u"""
-create table cells (
-  id integer PRIMARY KEY AUTOINCREMENT,
-  name varchar(10) UNIQUE,
-  region varchar(30),
-  voxel integer,
-  x integer,
-  y integer,
-  z integer
-);
-"""
+cur = conn.cursor()
 
-conn.execute(sql)
+cur.execute("""create table cells (id integer PRIMARY KEY AUTOINCREMENT,name varchar(10) UNIQUE,region varchar(30),voxel integer,x integer,y integer,z integer);""")
+
+cur.execute("""create table links (id integer PRIMARY KEY AUTOINCREMENT,fromcell varchar(10),tocell varchar(10),weight float);""")
 
 conn.close()
--- db/scripts/select.py
+++ db/scripts/select.py
@@ -2,11 +2,11 @@
 import csv
 import sqlite3
 
-conn = sqlite3.connect('cells.db')
+conn = sqlite3.connect('../cells.db')
 
 c = conn.cursor()
 
-sql = u"select * from cells"
+sql = u"select * from links"
 
 c.execute(sql)
 

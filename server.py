044c560608a0fc1f4bfb114346eb4da2ef24269e
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-03-18 15:10:52 +0900

    Add function to control traffic

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask.ext.socketio import SocketIO, send, emit
 import math
 import argparse
 import random
-
+import threading
 
 # Global variables
 app = Flask(__name__)
@@ -13,6 +13,9 @@ socketio = SocketIO(app)
 
 DATABASE = 'db/cells.db'
 abortrate = 0
+
+original_request_permission = {}
+request_permission = {}
 
 
 # Database Functions
@@ -38,6 +41,12 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+# Request validation
+def reset_permission():
+    global request_permission
+    request_permission = dict(original_request_permission)
+    t = threading.Timer(0.5, reset_permission)
+    t.start()
 
 # Routing Functions
 @app.route('/')
@@ -49,10 +58,12 @@ def index():
     # Data to send to view
     send_cells = {}
     send_links = []
+    global original_request_permission
     
     # Set data
     for cellname in cellnames:
         name = cellname["name"]
+        original_request_permission[name] = True
         send_cells[name] = query_db('select fullname,region,voxel,x,y,z from cells where name="%s"' % name)[0]
 
     for link in links:
@@ -70,19 +81,25 @@ def api():
         for d in data["cells"]:
             name = query_db('select name from cells where name="%s"' % d)
             if len(name) != 0:
-                data_name.append(name[0]["name"])
-        socketio.emit('activation', data_name)
+                if request_permission[name[0]["name"]]:
+                    data_name.append(name[0]["name"])
+                    request_permission[name[0]["name"]] = False
+        if len(data_name) != 0:
+            socketio.emit('activation', data_name)
         return "Request was sended.\n"
     else:
         return "Request was aborted.\n"
 
 # Main process
 if __name__ == '__main__':
-    #Setup abortrate
+    # Setup abortrate
     parser = argparse.ArgumentParser()
     parser.add_argument('--abortrate', dest='abortrate', default=0, type=int)
     abortrate = parser.parse_args().abortrate
     
-    #Run
+    # Permission
+    reset_permission()
+
+    # Run
     app.debug = True
     socketio.run(app)

6b55d82b52a8be13f162d107948d0651d6546f3a
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-03-22 18:16:08 +0900

    Update api-code

--- test/api.py
+++ test/api.py
@@ -1,16 +1,15 @@
 import json
 import urllib2
-import time
 
-def send_request(module):
+def send_to_viewer(fired_module):
     req = urllib2.Request('http://localhost:5000/api')
     req.add_header('Content-Type', 'application/json')
     send_data = {
-        "cells":[module]
+        "cells":[fired_module]
     }
     res = urllib2.urlopen(req, json.dumps(send_data))
 
 # test code
 if __name__ == '__main__':
     fired_module = "CA1"
-    send_request(fired_module)
+    send_to_viewer(fired_module)

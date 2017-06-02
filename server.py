3270d403666e914a893282242a82005f57a25f11
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2017-06-02 17:23:54 +0900

    introduce pandas to deal with db instead of sqlite3

--- server.py
+++ server.py
@@ -1,139 +1,119 @@
-import sqlite3
-from flask import Flask, render_template, g, request
-from flask.ext.socketio import SocketIO, send, emit
-
-import commands
-import threading
-
 import argparse
 
-# Global variables
+import threading
+import pandas as pd
+
+from flask import Flask, render_template, request
+from flask.ext.socketio import SocketIO
+
+
+# parse command line argument
+parser = argparse.ArgumentParser()
+parser.add_argument('--light-mode', dest='light_mode', action='store_true',
+                    help='reduce links to draw')
+args = parser.parse_args()
+
+# server settings
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bicamon'
 socketio = SocketIO(app)
 
-DATABASE = 'db/cells.db'
+# path to database
+PATH_CELLS = 'db/cells.csv'
+PATH_LINKS = 'db/links.csv'
+PATH_C_LINKS = 'db/contra-links.csv'
 
-request_permission = []
+# shared variables
+cell_list = []  # a list of cells
+send_cells = {}  # a dictionary that maps name into side information
+send_links = []  # a list which includes `root`, `dest`, and `weight`
+send_c_links = []  # a list which includes `root`, `dest`, and `weight`
+unable_to_send = []  # a list of cells which have been sent to the viewer
 
-send_cells = {}
-send_links = []
-send_c_links = []
 
-
-# Parse command line argument
-parser = argparse.ArgumentParser()
-parser.add_argument('--no-db-build', dest='no_build_db', action='store_true', help='skip db-build process')
-parser.add_argument('--light-mode', dest='light_mode', action='store_true', help='reduce links to draw')
-args = parser.parse_args()
-
-# Database Functions
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        db.row_factory = make_dicts
-    return db
-
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-def make_dicts(cursor, row):
-    return dict((cursor.description[idx][0], value)
-                for idx, value in enumerate(row))
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query ,args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-# Request validation
 def reset_permission():
-    global request_permission
-    request_permission = []
+    """Clear `unable_to_send`."""
+    global unable_to_send
+    unable_to_send = []
     t = threading.Timer(1.0, reset_permission)
     t.start()
 
 
-# Initialize
 def initialize():
+    """Extract information from database."""
     with app.app_context():
         # Global variables
-        global request_permission
+        global unable_to_send
         global send_cells
         global send_links
         global send_c_links
 
-        # Build database
-        if args.no_build_db:
-            pass
-        else:
-            check = commands.getoutput("./db/makedb.sh")
+        cells_cols = ['name', 'fullname', 'region', 'voxel', 'x', 'y', 'z']
+        cells = pd.read_table(PATH_CELLS, sep=',', names=cells_cols)
 
-        # Get data from database
-        cellnames = query_db('select name from cells')
-        links = query_db('select * from links')
-        contra_links = query_db('select * from contra_links')
+        links_cols = ['root', 'dest', 'weight']
+        links = pd.read_table(PATH_LINKS, sep=',', names=links_cols)
+        contra_links = pd.read_table(PATH_C_LINKS, sep=',', names=links_cols)
 
-        # Set data
-        for cellname in cellnames:
-            name = cellname["name"]
-            send_cells[name] = query_db('select fullname,region,voxel,x,y,z from cells where name="%s"' % name)[0]
+        for index, cell in cells.iterrows():
+            name = cell['name']
+            cell_list.append(name)
+            send_cells[name] = dict(cell[1:])
 
-        for link in links:
-            root_coordinate = query_db('select * from cells where name="%s"' % link["root"])
-            dest_coordinate = query_db('select * from cells where name="%s"' % link["dest"])
-            if len(root_coordinate) != 0 and len(dest_coordinate) != 0:
+        for index, link in links.iterrows():
+            if link['root'] in list(cells['name']) and link['dest'] in list(cells['name']):
                 if args.light_mode:
                     if link['weight'] > 0.1:
-                        send_links.append(link)
+                        send_links.append(dict(link))
                 else:
-                    send_links.append(link)
+                    send_links.append(dict(link))
 
-        for link in contra_links:
-            root_coordinate = query_db('select * from cells where name="%s"' % link["root"])
-            dest_coordinate = query_db('select * from cells where name="%s"' % link["dest"])
-            if len(root_coordinate) != 0 and len(dest_coordinate) != 0:
+        for index, link in contra_links.iterrows():
+            if link['root'] in list(cells['name']) and link['dest'] in list(cells['name']):
                 if args.light_mode:
                     if link['weight'] > 0.1:
-                        send_c_links.append(link)
+                        send_c_links.append(dict(link))
                 else:
-                    send_c_links.append(link)
-                
+                    send_c_links.append(dict(link))
+
+
 # Routing Functions
 @app.route('/')
 def index():
-    return render_template('index.html', cells=send_cells, links=send_links, c_links=send_c_links)
+    """Returns `index.html`."""
+    return render_template('index.html', cells=send_cells,
+                           links=send_links, c_links=send_c_links)
+
 
 @app.route('/api', methods=['GET', 'POST'])
 def api():
-    # Global variables
-    global request_permission
+    """Accept POST request and send requests to BiCAmon."""
+    global unable_to_send
 
     if request.method == 'POST':
-        data =  request.json
+        data = request.json
         data_name = []
-        name = query_db('select name from cells where name="%s"' % data["cells"][0])
-        if len(name) != 0 and not name[0]["name"] in request_permission:
-            data_name.append(name[0]["name"])
-            request_permission.append(name[0]["name"])
-        if len(data_name) != 0:
-            socketio.emit('activation', data_name)
-        return "Request was sended.\n"
+        for name in data['cells']:
+            if name in cell_list and name not in unable_to_send:
+                data_name.append(name)
+                unable_to_send.append(name)
+                socketio.emit('activation', data_name)
+            else:
+                continue
+        message = "Request was sended.\n"
     else:
-        return "Request was aborted.\n"
+        message = "Request was aborted.\n"
 
-# Main process
+    return message
+
+
 if __name__ == '__main__':
-    # Initialize
+    # extract information from db
     initialize()
 
-    # Permission control
+    # control traffic
     t = threading.Thread(target=reset_permission)
     t.start()
-    
-    # Run    
+
+    # launch an app
     socketio.run(app)
